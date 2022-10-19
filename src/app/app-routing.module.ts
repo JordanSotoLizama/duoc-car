af376d3925bb061fde4f473ae81e1e86558785fb
jordanSoto <contacto@jordanSoto.cl>
2022-10-19 04:07:37 -0500

    agregando ruta protegida y page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,37 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { DbService } from './services/db.service';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'animacion',
-    loadChildren: () => import('./pages/animacion/animacion.module').then( m => m.AnimacionPageModule)
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate:[DbService]
+  },
+  {
+    path: 'animacion',
+    loadChildren: () => import('./pages/animacion/animacion.module').then( m => m.AnimacionPageModule),
+    canActivate:[DbService]
+  },
+  {
+    path: 'e404',
+    loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'e404',
+    pathMatch: 'full'
+  },
+
 ];
 
 @NgModule({
